1a377ac3420a71b7b36d23301deef5088f93f756
kirkliston-iain <iainm50@googlemail.com>
2026-09-13 00:39:17 +0100

    Add files via upload

---
+++ src/pages/App.jsx
@@ -0,0 +1,53 @@
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './pages/Home'
+import CompetitionsIndex from './pages/CompetitionsIndex'
+import StandingsPage from './pages/StandingsPage'
+import Competition from './pages/Competition'
+import FixtureDetail from './pages/FixtureDetail'
+import TeamDetail from './pages/TeamDetail'
+import ScorersPage from './pages/ScorersPage'
+import HonoursPage from './pages/HonoursPage'
+import AdminLogin from './pages/AdminLogin'
+import AdminDashboard from './pages/AdminDashboard'
+import Discipline from './pages/Discipline'
+
+export default function App() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <Header />
+      <main style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/competitions" element={<CompetitionsIndex />} />
+          <Route path="/standings" element={<StandingsPage />} />
+          <Route path="/competitions/:slug" element={<Competition />} />
+          <Route path="/fixtures/:id" element={<FixtureDetail />} />
+          <Route path="/teams/:id" element={<TeamDetail />} />
+          <Route path="/scorers" element={<ScorersPage />} />
+          <Route path="/honours" element={<HonoursPage />} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route
+            path="/admin/dashboard"
+            element={
+              <ProtectedRoute>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/discipline"
+            element={
+              <ProtectedRoute>
+                <Discipline />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
