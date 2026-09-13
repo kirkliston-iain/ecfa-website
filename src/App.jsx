8d2d15cf4eb41aa9f732371c3b20b17ef6564363
kirkliston-iain <iainm50@googlemail.com>
2026-09-13 09:46:07 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ManagerGate from './components/ManagerGate'
 import ProtectedRoute from './components/ProtectedRoute'
 import Home from './pages/Home'
 import CompetitionsIndex from './pages/CompetitionsIndex'
@@ -12,6 +13,7 @@ import ScorersPage from './pages/ScorersPage'
 import HonoursPage from './pages/HonoursPage'
 import AdminLogin from './pages/AdminLogin'
 import AdminDashboard from './pages/AdminDashboard'
+import Discipline from './pages/Discipline'
 
 export default function App() {
   return (
@@ -36,9 +38,18 @@ export default function App() {
               </ProtectedRoute>
             }
           />
+          <Route
+            path="/discipline"
+            element={
+              <ProtectedRoute>
+                <Discipline />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </main>
       <Footer />
+      <ManagerGate />
     </div>
   )
 }
